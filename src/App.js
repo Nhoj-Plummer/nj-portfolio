d6c79fe913b9e06476ea51379cce9f9dc7b4e2c3
Nhoj <nhojplummer@yahoo.com>
2023-06-04 16:52:19 -0400

    Tetsing the connections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
+import "./App.css"
+import { Test } from "./components"
 
 function App() {
   return (
     <div className="App">
-      
+      <Test />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
