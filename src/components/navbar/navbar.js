811483d8da0eb9e424d9db08d5846d4ed6ac2e20
Nhoj <nhojplummer@yahoo.com>
2023-06-06 15:25:54 -0400

    Adding the menu possibly

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import "./navbar.css"
 
 function Navbar() {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-container"></div>
+        <div className="navbar-container">
+          <div className="menu-icon">
+            <i className={click ? "evn evn-times" : "evn evn-bars"}></i>
+          </div>
+        </div>
       </nav>
     </>
   )
